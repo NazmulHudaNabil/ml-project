8f2af72662bb71c30a38449b65d5f98e73a52e8d
NazmulHudaNabil <nabil648777@gmail.com>
2026-04-18 20:53:04 +0600

    data ingestion create

--- src/loger.py
+++ src/loger.py
@@ -2,19 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-# Create logs folder
-log_dir = os.path.join(os.getcwd(), "logs")
-os.makedirs(log_dir, exist_ok=True)
-
-# Create log filename with timestamp
+# create file name
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
+# create logs folder path
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# full file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.INFO
 )
 
 if __name__ == "__main__":
-    logging.info("Logging setup completed successfully!")
+    logging.info("loger file Created")
